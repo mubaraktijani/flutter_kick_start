f932201c9eceef820a7a9c78aafe8b332ccb29f4
mubaraktijani <starclick192@gmail.com>
2021-12-19 16:48:34 +0100

    folder restructure

--- lib/flutter_kick_start.dart
+++ lib/flutter_kick_start.dart
@@ -1,4 +1,15 @@
 library flutter_kick_start;
 
-export 'src/functions.dart';
-export 'src/jwt_helper.dart';
+export 'package:velocity_x/velocity_x.dart';
+export 'package:extended_image/extended_image.dart';
+export 'package:flutter_vector_icons/flutter_vector_icons.dart';
+
+export 'src/widgets/image.dart';
+export 'src/widgets/route_page.dart';
+export 'src/widgets/select_form_field.dart';
+export 'src/widgets/password_form_field.dart';
+export 'src/widgets/video_player/video_player.dart';
+
+export 'src/handlers/sizes.dart';
+export 'src/handlers/functions.dart';
+export 'src/handlers/jwt_helper.dart';
